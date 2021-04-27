1d5cffcd1a768cf0d2b6a9c6901448002a1d3705
Ignacio Borassi <Nachitobora>
2021-04-27 14:22:51 -0300

    Agregado el 8, con sus comentarios

--- ej8.py
+++ ej8.py
@@ -1,2 +1,20 @@
-#aca iria el ejercicio 8
+romanos = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+
+def conversor(entero):         #Convierte el numero entero en romano
+    resultado = ""
+
+    while entero > 0:
+        for n, r in romanos:
+            while entero > n or entero == n:
+                resultado = resultado + r
+                entero = entero - n
+    return resultado
+
+entero = int(input("Ingrese un numero entero: "))
+if entero < 0:                     #Checker si es valido o no
+    "Ingreso un numero entero no valido"
+elif entero == 0:
+    "No se puede representar el 0 en la numerologia romana"
+else: 
+    print(conversor(entero)) 
 
