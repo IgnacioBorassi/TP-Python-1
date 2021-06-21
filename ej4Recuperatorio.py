05405013f14475388da697178cadb6279c6d1bd3
Ignacio Borassi <Nachitobora>
2021-06-21 13:02:23 -0300

    Agrego ejercicio 4 del recuperatorio

--- ej4Recuperatorio.py
+++ ej4Recuperatorio.py
@@ -1,37 +1,19 @@
-import random
-from random import randrange
-tiradas = int(input("Ingrese cantidad de tiradas: "))
-def dados(tiradas):
-    uno = 0
-    dos = 0
-    tres = 0
-    cuatro = 0
-    cinco = 0
-    seis = 0
-    for i in range(tiradas * 2):
-        random = randrange(1,6)
-        if random == 1:
-            uno = uno + 1
-        if random == 2:
-            dos = dos + 1 
-        if random == 3:
-            tres = tres + 1
-        if random == 4:
-            cuatro = cuatro + 1
-        if random == 5:
-            cinco = cinco + 1  
-        if random == 6: 
-            seis = seis + 1   
-    if uno != 0:
-        print("El numero 1 salio: ", uno, "veces")
-    if dos != 0:
-        print("El numero 2 salio: ", dos, "veces")
-    if tres != 0:
-        print("El numero 3 salio: ", tres, "veces")
-    if cuatro != 0:
-        print("El numero 4 salio: ", cuatro, "veces")
-    if cinco != 0:
-        print("El numero 5 salio: ", cinco, "veces")
-    if seis != 0:
-        print("El numero 6 salio: ", seis, "veces")
-    return tiradas
+def validar(calculo):
+    """Valida una cadena de calculos matematicos para saber si los simbolos como corchetes, parentesis y llaves, estan bien colocados"""
+    cadena = []
+    abiertos = "({["
+    cerrados = ")}]"
+
+    for i in calculo:
+        if i in abiertos:       #Si el simbolo abierto esta en la cadena, lo mete en otra lista aparte.
+            cadena.append(i)
+        elif i in cerrados:     #Si el simbolo cerrado esta, y la cadena no tiene otros caracteres, es falso, ya que no tiene qué cerrar.
+            if len(cadena) == 0:
+                return False
+            else:               #Pero si hay un sibolo abriendo, verifica que sea el mismo tipo (parentesis, corchete o llave).   
+                simbolo = cadena.pop()    
+                tipoabierto = abiertos[cerrados.index(i)]   #La posicion del cerrado, determina la de abierto. Lo igualo en otra variable para comparar 
+                if simbolo != tipoabierto:        #Si son diferentes, el simbolo de cierre es diferente al de apertura, por lo tanto: False     
+                    return False
+
+    return not len(cadena)
